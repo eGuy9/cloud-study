92f48806cce8a4b47eddb7524d88b72d07319443
yyds <1321241365@qq.com>
2023-09-05 20:50:35 +0800

    eureka commit

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderController {
+    String url = "http://cloud-payment-service";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+
+        return restTemplate.postForObject(url+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return restTemplate.getForObject(url+"/payment/get/"+id,CommonResult.class);
+    }
+
+}
